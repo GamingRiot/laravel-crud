f067dceaa6fac977447904db84ba3c8345bcb978
Tushar <gamingriot56@gmail.com>
2023-12-02 02:39:29 +0530

    error messages and past value storage

--- resources/views/Register/show.blade.php
+++ resources/views/Register/show.blade.php
@@ -13,25 +13,37 @@
       <!-- Name Field -->
       <div class="mb-4">
           <label for="name" class="block text-gray-700 text-sm font-bold mb-2">Name</label>
-          <input type="text" id="name" name="name" class="w-full px-3 py-2 border rounded-md focus:outline-none focus:border-blue-500" placeholder="John Doe" required>
+          <input type="text" id="name" name="name" class="w-full px-3 py-2 border rounded-md focus:outline-none focus:border-blue-500" placeholder="John Doe" value="{{old('name')}}" required>
+          @error('name')
+            <p class="text-red-500">{{$message}}</p>
+          @enderror
       </div>
 
       <!-- Username Field -->
       <div class="mb-4">
           <label for="username" class="block text-gray-700 text-sm font-bold mb-2">Username</label>
-          <input type="text" id="username" name="username" class="w-full px-3 py-2 border rounded-md focus:outline-none focus:border-blue-500" placeholder="johndoe" required>
+          <input type="text" id="username" name="username" class="w-full px-3 py-2 border rounded-md focus:outline-none focus:border-blue-500" placeholder="johndoe" value="{{old('username')}}" required>
+          @error('username')
+            <p class="text-red-500">{{$message}}</p>
+          @enderror
       </div>
 
       <!-- Email Field -->
       <div class="mb-4">
           <label for="email" class="block text-gray-700 text-sm font-bold mb-2">Email</label>
-          <input type="email" id="email" name="email" class="w-full px-3 py-2 border rounded-md focus:outline-none focus:border-blue-500" placeholder="john.doe@example.com" required>
+          <input type="email" id="email" name="email" class="w-full px-3 py-2 border rounded-md focus:outline-none focus:border-blue-500" placeholder="john.doe@example.com" value="{{old('email')}}" required>
+          @error('email')
+            <p class="text-red-500">{{$message}}</p>
+          @enderror
       </div>
 
       <!-- Password Field -->
       <div class="mb-6">
           <label for="password" class="block text-gray-700 text-sm font-bold mb-2">Password</label>
           <input type="password" id="password" name="password" class="w-full px-3 py-2 border rounded-md focus:outline-none focus:border-blue-500" placeholder="********" required>
+          @error('password')
+            <p class="text-red-500">{{$message}}</p>
+          @enderror
       </div>
 
       <!-- Submit Button -->
