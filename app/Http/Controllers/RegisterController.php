12b4c85880b5ae7c2cbe6367c88916981edb76dd
Tushar <gamingriot56@gmail.com>
2023-12-02 02:41:45 +0530

    unique validation

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Redirect;
+use Illuminate\Validation\Rule;
 
 class RegisterController extends Controller
 {
@@ -16,8 +17,8 @@ class RegisterController extends Controller
   {
     $valiidAuthentication = request()->validate([
       'name' => ['required', 'min:4', 'max:255'],
-      'username' => ['required', 'min:4', 'max:255'],
-      'email' => ['required', 'min:4', 'max:255', 'email'],
+      'username' => ['required', 'min:4', 'max:255', Rule::unique('users', 'username')],
+      'email' => ['required', 'min:4', 'max:255', 'email', Rule::unique('users', 'email')],
       'password' => ['required', 'min:4', 'max:255'],
     ]);
 
